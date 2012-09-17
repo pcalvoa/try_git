50740266fc7298f2ac037ec0593bf4d4daf7b190
pcalvoa <pcalvoa@oesia.com>
2012-09-17 13:45:11 +0200

    My first class

---
+++ MyJavaProject/src/Hello.java
@@ -0,0 +1,23 @@
+
+
+
+public class Hello {
+	
+	public Hello(){
+		
+	}
+	
+	public String writeHello(){
+		return "hello";
+	}
+	
+	static public void main(String[] argv){
+		
+		Hello h = new Hello();
+		
+		System.out.println(h.writeHello());
+				
+		
+	}
+	
+}
